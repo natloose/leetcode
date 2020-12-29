fb1ea010a1671cdc6a58e91f7e4c9291ef95f58f
conorwilliams <conorpatrickwilliams@gmail.com>
2020-12-29 12:39:18 +0000

    Q7 alternative solution + notes

--- lc/Q7_reverse _int.py
+++ lc/Q7_reverse _int.py
@@ -1,7 +1,33 @@
 # 7. Reverse Integer
 
+x = 1009
+
+# First Solution (While Loop)
+
 
 def reverse(x):
+    if x >= 2 ** 31 - 1 or x < -2 ** 31:
+        print(0)
+    else:
+        minus = -1 if x < 0 else 1
+        x = x * minus
+        reverse = 0
+
+        while x > 0:
+            remainder = x % 10
+            reverse = (reverse * 10) + remainder
+            x = x // 10
+
+        if reverse >= 2 ** 31 - 1 or x < -2 ** 31:
+            print(0)
+        else:
+            print(reverse * minus)
+
+
+reverse(x)
+
+# Second Solution
+'''def reverse(x):
     if x >= 2 ** 31-1 or x < -2 ** 31: return 0
     else:
         # Stipulate the variable minus. If x is a negative number minus = -1 else = 1
@@ -28,4 +54,6 @@ def reverse(x):
         else:
             print(minus*x)
 
-reverse(123)
+reverse(123)'''
+
+
